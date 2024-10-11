35ac50c088f8fbcea28bf70ad9ea194d6f1d85f7
Jan Mikeš <j.mikes@me.com>
2024-10-11 11:15:02 +0200

    Fix fixtures

--- tests/DataFixtures/TestDataFixture.php
+++ tests/DataFixtures/TestDataFixture.php
@@ -7,10 +7,7 @@ use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Ramsey\Uuid\Uuid;
-use TheDevs\WMS\Entity\Manual;
-use TheDevs\WMS\Entity\Project;
 use TheDevs\WMS\Entity\User;
-use TheDevs\WMS\Value\ManualType;
 
 final class TestDataFixture extends Fixture
 {
@@ -19,12 +16,6 @@ final class TestDataFixture extends Fixture
 
     public const string USER_2_ID = '00000000-0000-0000-0000-000000000002';
     public const string USER_2_EMAIL = 'user2@test.cz';
-
-    public const string PROJECT_1_ID = '00000000-0000-0000-0000-000000000001';
-    public const string PROJECT_2_ID = '00000000-0000-0000-0000-000000000002';
-
-    public const string MANUAL_1_ID = '00000000-0000-0000-0000-000000000001';
-    public const string MANUAL_2_ID = '00000000-0000-0000-0000-000000000002';
 
     public function load(ObjectManager $manager): void
     {
@@ -38,24 +29,6 @@ final class TestDataFixture extends Fixture
         );
         $manager->persist($user1);
 
-        $project1 = new Project(
-            Uuid::fromString(self::PROJECT_1_ID),
-            $user1,
-            $date,
-            'Project 1',
-        );
-        $manager->persist($project1);
-
-        $manual1 = new Manual(
-            Uuid::fromString(self::MANUAL_1_ID),
-            $project1,
-            $date,
-            ManualType::Logo,
-            'Manual 1',
-            null,
-        );
-        $manager->persist($manual1);
-
         $user2 = new User(
             Uuid::fromString(self::USER_2_ID),
             self::USER_2_EMAIL,
@@ -63,24 +36,6 @@ final class TestDataFixture extends Fixture
             true,
         );
         $manager->persist($user2);
-
-        $project2 = new Project(
-            Uuid::fromString(self::PROJECT_2_ID),
-            $user2,
-            $date,
-            'Project 2',
-        );
-        $manager->persist($project2);
-
-        $manual2 = new Manual(
-            Uuid::fromString(self::MANUAL_2_ID),
-            $project2,
-            $date,
-            ManualType::Logo,
-            'Manual 2',
-            null,
-        );
-        $manager->persist($manual2);
 
         $manager->flush();
     }
