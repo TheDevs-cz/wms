5570c0b1c2093267ad8dc04c2d34a59a03c7bdb4
Jan Mikeš <j.mikes@me.com>
2024-10-12 00:13:16 +0200

    Draft of navbar

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
+// Twitter Bootstrap JS
+import 'bootstrap';
+
 import './styles/app.css';
 
-import './bootstrap.js'
+import './bootstrap.js';
