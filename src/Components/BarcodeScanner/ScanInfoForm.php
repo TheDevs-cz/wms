569055b3b4758d9799e41349fa7eca715d809e1d
Jan Mikeš <j.mikes@me.com>
2024-10-15 20:02:02 +0200

    Basic scanning works

--- src/Components/BarcodeScanner/ScanInfoForm.php
+++ src/Components/BarcodeScanner/ScanInfoForm.php
@@ -9,16 +9,27 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use TheDevs\WMS\Exceptions\ProductNotFound;
 use TheDevs\WMS\FormData\BarcodeScanFormData;
 use TheDevs\WMS\FormType\BarcodeScanFormType;
+use TheDevs\WMS\Query\ProductQuery;
 
 #[AsLiveComponent]
-final class BarcodeScannerForm extends AbstractController
+final class ScanInfoForm extends AbstractController
 {
     use DefaultActionTrait;
     use ComponentWithFormTrait;
+
+    #[LiveProp]
+    public bool $formSubmitted = false;
+
+    public function __construct(
+        readonly private ProductQuery $productQuery,
+    ) {
+    }
 
     /**
      * @return FormInterface<BarcodeScanFormData>
@@ -31,7 +42,7 @@ final class BarcodeScannerForm extends AbstractController
     }
 
     #[LiveAction]
-    public function handleScan(): Response
+    public function handleScan()
     {
         $this->submitForm();
 
@@ -42,6 +53,12 @@ final class BarcodeScannerForm extends AbstractController
             return $this->redirect($data->code);
         }
 
-        return $this->redirectToRoute('scan_info');
+        try {
+            $product = $this->productQuery->getByEan($data->code);
+
+            return $this->redirectToRoute('product_info', ['id' => $product->id]);
+        } catch (ProductNotFound) {
+            $this->formSubmitted = true;
+        }
     }
 }
