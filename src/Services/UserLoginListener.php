36d9b8228b7eb58504aebfe3fff7008f067de439
Jan Mikeš <j.mikes@me.com>
2024-11-21 00:12:49 +0100

    Last Login - change event type

--- src/Services/UserLoginListener.php
+++ src/Services/UserLoginListener.php
@@ -5,14 +5,10 @@ declare(strict_types=1);
 namespace TheDevs\WMS\Services;
 
 use Psr\Clock\ClockInterface;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 use TheDevs\WMS\Entity\User;
-use TheDevs\WMS\Message\User\LogUserActivity;
 use TheDevs\WMS\Message\User\LogUserLogin;
 
 #[AsEventListener]
@@ -24,10 +20,10 @@ readonly final class UserLoginListener
     ) {
     }
 
-    public function __invoke(LoginSuccessEvent $event): void
+    public function __invoke(InteractiveLoginEvent $event): void
     {
         /** @var User $user */
-        $user = $event->getUser();
+        $user = $event->getAuthenticationToken()->getUser();
 
         $this->messageBus->dispatch(
             new LogUserLogin(
